069c0836ed0c7c758f1666221182a05dd5b6b019
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-03-22 10:46:58 +0530

    moved main.rb to bin/. removed controller to reduc complexity. init rspec

--- main.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'lib/controller.rb'
-
-controller = Controller.new
-
-controller.run
---
+++ spec/scraper_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper.rb'
+
+RSpec.describe Scraper do
+  subject { Scraper.new }
+
+  describe '#scrape' do
+    
+  end
+end
